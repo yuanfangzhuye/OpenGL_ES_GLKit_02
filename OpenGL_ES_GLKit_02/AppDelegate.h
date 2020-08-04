28b2984d19c5a00eaacc6cf4aca1224b989d74e1
chao.li <chao.li@team-lab.cn>
2020-08-04 10:53:39 +0800

    Initial Commit

---
+++ OpenGL_ES_GLKit_02/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGL_ES_GLKit_02
+//
+//  Created by tlab on 2020/8/4.
+//  Copyright © 2020 yuanfangzhuye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OpenGL_ES_GLKit_02/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  OpenGL_ES_GLKit_02
+//
+//  Created by tlab on 2020/8/4.
+//  Copyright © 2020 yuanfangzhuye. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
